a671659880373293da6b1eac592b3cece43173cd
kouwa stephane <kksasteph888@gmail.com>
2022-12-13 12:38:37 +0100

    Maj

--- src/Plugin/Field/FieldFormatter/ValueNiveauFormatterType.php
+++ src/Plugin/Field/FieldFormatter/ValueNiveauFormatterType.php
@@ -47,7 +47,6 @@ class ValueNiveauFormatterType extends FormatterBase {
   public function settingsSummary() {
     $summary = [];
     // Implement settings summary.
-    
     return $summary;
   }
   
@@ -57,14 +56,35 @@ class ValueNiveauFormatterType extends FormatterBase {
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
+    $taxonomy_term = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
+    /**
+     * Il faut urgenment trouver le moyen de recuperer cette configuration à
+     * partir des données du widget type.
+     *
+     * @var array $niveau
+     */
+    $niveau = [
+      1 => 'Faible',
+      2 => 'Base',
+      3 => 'Moyen',
+      4 => 'Bien',
+      5 => 'Excellent'
+    ];
     foreach ($items as $delta => $item) {
-      $elements[$delta] = [
-        '#theme' => 'buildercv_value_niveau_formatter',
-        '#item' => [
-          'target_id' => $item->target_id,
-          'niveau' => $item->niveau
-        ]
-      ];
+      $term = $taxonomy_term->load($item->target_id);
+      $name = null;
+      if ($term) {
+        $name = $term->label();
+      }
+      if ($name)
+        $elements[$delta] = [
+          '#theme' => 'buildercv_value_niveau_formatter',
+          '#item' => [
+            'target_id' => $item->target_id,
+            'niveau' => isset($niveau[$item->niveau]) ? $niveau[$item->niveau] : null,
+            'name' => $name
+          ]
+        ];
     }
     return $elements;
   }
