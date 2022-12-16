3a4498a23160f57764ab398ac4b34e1dda446f6b
kouwa stephane <kksasteph888@gmail.com>
2022-12-16 22:53:56 +0100

    maj

--- src/Form/CvEntitySettingsForm.php
+++ src/Form/CvEntitySettingsForm.php
@@ -11,43 +11,42 @@ use Drupal\Core\Form\FormStateInterface;
  * @ingroup buildercv
  */
 class CvEntitySettingsForm extends FormBase {
-
+  
   /**
    * Returns a unique string identifying the form.
    *
-   * @return string
-   *   The unique string identifying the form.
+   * @return string The unique string identifying the form.
    */
   public function getFormId() {
     return 'cventity_settings';
   }
-
+  
   /**
    * Form submission handler.
    *
    * @param array $form
-   *   An associative array containing the structure of the form.
+   *        An associative array containing the structure of the form.
    * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
+   *        The current state of the form.
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Empty implementation of the abstract submit class.
   }
-
+  
   /**
    * Defines the settings form for Cv entity entities.
    *
    * @param array $form
-   *   An associative array containing the structure of the form.
+   *        An associative array containing the structure of the form.
    * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
-   *
-   * @return array
-   *   Form definition array.
+   *        The current state of the form.
+   *        
+   * @return array Form definition array.
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    // dump($form);
     $form['cventity_settings']['#markup'] = 'Settings form for Cv entity entities. Manage field settings here.';
     return $form;
   }
-
+  
 }
