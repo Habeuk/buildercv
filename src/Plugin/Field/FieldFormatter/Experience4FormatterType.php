e918805d31222675c30b20661dd509a3af0f5ed9
vystii <ngahabanda@gmail.com>
2023-02-21 12:41:56 +0100

    creation text-bg et experience4

---
+++ src/Plugin/Field/FieldFormatter/Experience4FormatterType.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\buildercv\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'experience_formatter_type' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "experience4_formatter_type",
+ *   label = @Translation("Experience formatter model avec les dates en bas à droite "),
+ *   field_types = {
+ *     "experience_type"
+ *   }
+ * )
+ */
+class Experience3FormatterType extends ExperienceFormatterType
+{
+
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public static function defaultSettings()
+  {
+    return [
+      'layoutgenentitystyles_view' => 'buildercv/field-exp4'
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state)
+  {
+    return [
+      // utilile pour mettre à jour le style
+      'layoutgenentitystyles_view' => [
+        '#type' => 'hidden',
+        '#value' => 'buildercv/field-exp4'
+      ]
+    ] + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode)
+  {
+    $elements = [];
+    foreach ($items as $delta => $item) {
+      $date_debut = '';
+      if (!empty($item->date_debut)) {
+        $date_debut = DrupalDateTime::createFromTimestamp($item->date_debut);
+        $date_debut = $date_debut->format("m/Y");
+      }
+      $date_fin = '';
+      if (!empty($item->date_fin)) {
+        $date_fin = DrupalDateTime::createFromTimestamp($item->date_fin);
+        $date_fin = $date_fin->format("m/Y");
+      }
+      $elements[$delta] = [
+        '#theme' => 'buildercv_experience_formatter4',
+        '#item' => [
+          'value' => Html::escape($item->value),
+          'company' => Html::escape($item->company),
+          'address' => Html::escape($item->address),
+          'date_debut' => $date_debut,
+          'date_fin' => $date_fin,
+          'description' => $item->description,
+        ]
+      ];
+    }
+    return $elements;
+  }
+}
---
+++ src/Plugin/Field/FieldFormatter/TextTitleBGFormatter.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\buildercv\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\Plugin\Field\FieldFormatter\StringFormatter;
+
+/**
+ * Plugin implementation of the 'string' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "string",
+ *   label = @Translation("Plain text"),
+ *   field_types = {
+ *     "string",
+ *   }
+ * )
+ */
+class TextTitleBGFormatter extends StringFormatter
+{
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public static function defaultSettings()
+  {
+    return [
+      'layoutgenentitystyles_view' => 'buildercv/field-text-bg'
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state)
+  {
+    return [
+      // utilile pour mettre à jour le style
+      'layoutgenentitystyles_view' => [
+        '#type' => 'hidden',
+        '#value' => 'buildercv/field-text-bg'
+      ]
+    ] + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode)
+  {
+    $elements = [];
+
+    // The ProcessedText element already handles cache context & tag bubbling.
+    // @see \Drupal\filter\Element\ProcessedText::preRenderText()
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = [
+        '#theme' => 'buildercv_text_bg',
+        '#item' => Html::escape($item->value)
+      ];
+    }
+
+    return $elements;
+  }
+}
